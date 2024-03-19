7ce6ed6428a444ecd070218637fdac0f0b015cf4
Joselu <joseluissgz97@gmail.com>
2024-03-19 14:26:52 +0100

    Task component

---
+++ src/components/tasks-component/Tasks.tsx
@@ -0,0 +1,11 @@
+import style from './Tasts.module.scss';
+
+const Tasks = () => {
+  return (
+    <section className={style.tasks}>
+      <span> Taks </span>
+    </section>
+  );
+};
+
+export default Tasks;
