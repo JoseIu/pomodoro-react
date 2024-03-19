e99a82213f8bd29af6d9ca86414896b53fb5131a
Joselu <joseluissgz97@gmail.com>
2024-03-19 14:26:15 +0100

    Break mode

---
+++ src/components/break/Break.tsx
@@ -0,0 +1,10 @@
+import BreakItcon from '../icons/BreakItcon';
+
+import style from './Breadk.module.scss';
+
+const Break = () => (
+  <span className={style.work__break}>
+    Break <BreakItcon className={style.icon} />
+  </span>
+);
+export default Break;
