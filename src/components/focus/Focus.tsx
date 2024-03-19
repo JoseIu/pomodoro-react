5f6d1ea1db59efa1d7b252c1450ad242a530ead0
Joselu <joseluissgz97@gmail.com>
2024-03-19 14:26:22 +0100

    Focus mode

---
+++ src/components/focus/Focus.tsx
@@ -0,0 +1,10 @@
+import FocusIcon from '../icons/FocusIcon';
+import style from './Focus.module.scss';
+
+const Focus = () => (
+  <span className={style.work__focus}>
+    Focus <FocusIcon className={style.icon} />
+  </span>
+);
+
+export default Focus;
